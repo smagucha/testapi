11fadc55f3d8234607a0616c5ad7813cbc69fdf9
smagucha <smagucha@gmail.com>
2021-02-02 12:38:46 +0300

    migrations

--- firstapi/first/models.py
+++ firstapi/first/models.py
@@ -1,3 +1,5 @@
 from django.db import models
-
-# Create your models here.
+class Article(models.Model):
+    name = models.CharField(max_length=100)
+    email = models.EmailField()
+    comment = models.TextField()
