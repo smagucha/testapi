13cff2304bc982122f5f8235ada1cfaf733ce3f6
smagucha <smagucha@gmail.com>
2021-02-02 15:39:27 +0300

    fucntion api

--- firstapi/first/urls.py
+++ firstapi/first/urls.py
@@ -4,5 +4,8 @@ from . import views
 urlpatterns = [
 
     path('', views.home),
-    path('article_list', views.article_list)
+    path('article_list', views.article_list),
+    path('detail/<int:pk>', views.article_detail),
+    #path('article_list', views.article_list),
+    #path('article_list', views.article_list),
 ]
